411be84ff8c4a7e55b17b6a762f212e979b54fa9
kokijaga <s2526@oita.kosen-ac.jp>
2026-06-02 09:49:09 +0900

    003

--- src/main.c
+++ src/main.c
@@ -1,2 +1,34 @@
 //担当 田邊
 //テスト002
+#include<stdio.h>
+#include"common.h"
+int main(void)
+{
+    int  id;
+    char title[64];
+    char deadline[11];  /* YYYY-MM-DD */
+    int  priority;      /* 1..5      */
+    int  done;          /* 0 or 1    */
+    
+
+    printf("=========素晴らしき課題管理ツール=========\n\n");
+
+    printf("追加する課題を入力して！\n");
+    scanf("%s",title);
+
+    printf("課題の締め切りを入力して！ (YYYY-MM-DD)\n");
+    scanf("%s",deadline);
+
+    printf("課題の優先度を入力して！ (1~5)\n");
+    scanf("%d",&priority);
+
+    
+
+
+    
+    
+    
+
+
+
+}
