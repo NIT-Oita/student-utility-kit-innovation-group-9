931587624051e636f77c0e16b1996b7c608109aa
hirapito <soshiroikeda0624@gmail.com>
2026-06-02 10:10:46 +0900

    ファイル入出力機工仮定完了

--- src/storage.c
+++ src/storage.c
@@ -5,4 +5,18 @@
 
 int main(void){
     printf("テストファイル生成\n");
+    FILE *fp;
+    fp = fopen("test.txt","w");
+    if (fp == NULL){
+        printf("ファイル作成エラー\n");
+        return 114514;
+    }
+
+    fprintf(fp,"テストテキスト入力\n");
+
+    fclose(fp);
+
+    printf("正常終了");
+
+    return 0;
 }
