39839a47563ffdcc2534d1dfe95c15c9672d1d04
hirapito <soshiroikeda0624@gmail.com>
2026-06-21 21:57:02 +0900

    ストレージとそれに伴う追加

--- src/common.h
+++ src/common.h
@@ -1,16 +1,23 @@
+common.h
 #ifndef LOGIC_H
 #define LOGIC_H
 #define MAX_TASKS 100
 #include <stdio.h>
-typedef struct {
-int  id;
-char title[64];
-char deadline[11];  /* YYYY-MM-DD */
-int  priority;      /* 1..5      */
-int  done;          /* 0 or 1    */
-} Task;
-int  task_urgency(const Task *t,
-const char *today);
-void task_sort(Task *list, int n);
-int storage();
+#include <windows.h>
+#include <string.h>
+#include <time.h>
+#include<string.h>
+typedef struct{
+    char name[1024];
+    char deadline[1024];
+    int yuusen;
+    int done;
+} DATA;
+ 
+int file_kakunin();
+int output();
+int input();
+int csv_kakikomi(DATA *task);
+int csv_yomikomi(DATA task[], int *count);
+ 
 #endif
